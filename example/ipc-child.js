e2e00d3ddd5ad4eb60e8455ed4a1a752d40df025
Marc Ablay <marc.ablay@gmail.com>
2021-12-23 23:56:30 +0100

    simplified IPC example

--- example/ipc-child.js
+++ example/ipc-child.js
@@ -1,29 +1,6 @@
 import { Rpc } from '../index.js'
-// class RpcRemoteEncodingError extends Error {}
-process.name = 'CHILD '
-const log = (...args) => console.log(process.name, '| main |', ...args)
-
 const rpc = Rpc.fromIpcProcess(process)
-main().catch(log)
-
-async function main () {
-
-  // setup RPC handler
-  rpc.handle('greet', greet)
-
-  // program
-  await remoteCalculation()
-  rpc.notify('done')
-}
-
-async function remoteCalculation () {
-  const [a, b] = [3, 2]
-  await rpc.request('add', [a, b])
-    .then(sum => log('received:', a, '+', b, '=', sum))
-    .catch(error => log(error.message))
-}
-
-function greet (subject) {
-  log('execute "greet"')
-  return `Hello ${subject}!`
-}
+rpc.request('add', [3, 5]).then(sum => {
+  console.log('3 + 5 =', sum)
+  rpc.close()
+})
--- example/ipc-parent.js
+++ example/ipc-parent.js
@@ -1,27 +1,5 @@
-import { fork } from 'child_process'
 import { Rpc } from '../index.js'
-
-process.name = 'PARENT'
-const log = (...args) => console.log(process.name, '| main |', ...args)
-
+import { fork } from 'child_process'
 const child = fork('./example/ipc-child.js')
 const rpc = Rpc.fromIpcProcess(child)
-
-// setup RPC handler
-rpc.handle('add', remoteAdd)
-rpc.handle('done', async () => {
-  const greeting = await rpc.request('greet', 'world')
-  log('received:', greeting)
-  child.kill()
-  process.exit()
-})
-
-// this method is executed remotely and responds via serialised messages
-async function remoteAdd ([a, b]) {
-  await new Promise(ok => setTimeout(ok, 500))
-  log('executing remoteAdd', a, b)
-  if (!Number.isFinite(b)) {
-    throw new Error('Add requires two finite number arguments!')
-  }
-  return a + b
-}
+rpc.handle('add', ([a, b]) => a + b) // also works with async functions
