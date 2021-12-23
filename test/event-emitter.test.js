40923a86637d8b85ae8aa0f18744c7b46d37e6fe
Marc Ablay <marc.ablay@gmail.com>
2021-12-23 23:07:51 +0100

    updated EventEmitter test and example

--- test/event-emitter.test.js
+++ test/event-emitter.test.js
@@ -2,6 +2,10 @@ import { test } from 'zora'
 import { EventEmitter } from '@occami/events'
 import { Rpc } from '../index.js'
 
+const wait = ms => new Promise(ok => setTimeout(ok, ms))
+const hold = (ms, x) => wait(ms).then(() => x)
+// const delay = (ms, fn) => wait(ms).then(fn)
+
 function rpcsFromEventEmitters() {
   const [a, b] = [new EventEmitter(), new EventEmitter()]
   const alice = Rpc.fromEventEmitters(a, b)
@@ -22,7 +26,7 @@ test ('notify', t => {
 
 test ('request (success)', async t => {
   const [a, b] = rpcsFromEventEmitters()
-  b.handle('add', ([x, y]) => x + y)
+  b.handle('add', ([x, y]) => hold(20, x + y))
   const sum = await a.request('add', [3, 5])
   t.equal(sum, 8, 'request has been handled successfully')
 })
