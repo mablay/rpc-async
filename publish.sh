5b05cecae981e89ade00d61918cd9a0292079dad
Marc Ablay <marc.ablay@gmail.com>
2023-04-26 12:11:04 +0200

    updated npmignore and deploy script

--- publish.sh
+++ publish.sh
@@ -4,15 +4,6 @@
 ## ./deploy.sh [major|minor|patch]
 
 echo "This script will increment the package version and publish it publicly"
-echo "Please review the packlist before publishing..."
-echo ""
-npx npm-packlist
-echo ""
-read -p "Confirm packlist? (y/N) " yn
-case $yn in
-  [Yy]* ) echo "packlist confirmed";;
-  * ) echo "packlist rejected"; exit;;
-esac
 
 v="${1:-patch}"
 # rm -rf node_modules
@@ -21,6 +12,17 @@ echo "building..."
 npm run build
 echo "testing..."
 npm test
+
+echo "Please review the packlist before publishing..."
+echo ""
+npx npm-packlist-cli
+echo ""
+read -p "Confirm packlist? (y/N) " yn
+case $yn in
+  [Yy]* ) echo "packlist confirmed";;
+  * ) echo "packlist rejected"; exit;;
+esac
+
 tag=`npm version $v`
 echo "publishing ($tag)..."
 git push && git push --tags
