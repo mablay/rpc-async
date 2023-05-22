c29e70d0b3464d8c92e965df922a2e9808d83d8d
Marc Ablay <marc.ablay@gmail.com>
2023-05-22 23:41:46 +0200

    fixed cluster example

--- example/cluster.ts
+++ example/cluster.ts
@@ -1,7 +1,7 @@
+#!/usr/bin/env tsx
 import cluster from 'node:cluster'
-import { Rpc } from '..'
-import { Alice } from './behavior/alice'
-import { Bob } from './behavior/bob'
+import { ipcProcessRpc } from '..'
+import { IpcPeer } from './behavior/process'
 
 if (cluster.isPrimary) {
   console.log(`[Primary] ${process.pid} is running`)
@@ -10,12 +10,23 @@ if (cluster.isPrimary) {
     console.log(`[Primary] worker ${worker.process.pid} died`)
   })
 
-  const local = new Alice()
-  const remote = Rpc.fromIpcProcess(worker)
-  // remote.handle()
+  const local = new IpcPeer()
+  const remote = ipcProcessRpc<IpcPeer>(worker).wrap(local, 2)
+  await new Promise(resolve => local.once('ready', resolve))
+  const sum = await remote.request.add(3, 5)
+  console.log('sum 3 + 5 =', sum)
+  remote.notify.log('foo')
+  const keys = await remote.request.put({ foo: 'bar', lorem: 'ipsum' })
+  console.log('keys:', keys)
+  // remote.notify.exit()
+  worker.kill()
 } else {
   console.log(`[Worker] ${process.pid} started`)
-  const local = new Bob()
-  const remote = Rpc.fromIpcProcess(process)
-
+  const local = new IpcPeer()
+  const remote = ipcProcessRpc<IpcPeer>(process) // .wrap(local)
+  remote.handle('add', (a: number, b: number) => local.add(a, b))
+  remote.handle('put', (data: any) => local.put(data))
+  remote.handle('log', (line: string) => local.log(line))
+  remote.handle('exit', () => local.exit())
+  remote.notify.ready()
 }
